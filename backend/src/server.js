db9e46e3f05510977ec2f292a2c360f5ad8fd602
gladysj-cloud <gladys.jacob@techjays.com>
2025-06-08 22:59:40 -0400

    Fixed errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,12 @@ const Student = require('./models/Student');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://algebratutor.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
